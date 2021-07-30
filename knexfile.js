fb9038fee1dc8ebb4ed6be276898ff496e8b6203
Andrew Jenkins <jenkins_aj@yahoo.com>
2021-07-30 12:50:02 +0100

    initial commit plus migrations added

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+if(process.env.USER) require("dotenv").config();
+
+const path = require("path")
+
+const {
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+} = process.env;
+
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+    pool: {min: 0, max: 5},
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+    pool: {min: 0, max: 5},
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: DATABASE_URL,
+    pool: {min: 0, max: 5},
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  }
+
+};
