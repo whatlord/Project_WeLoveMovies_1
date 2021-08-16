5ef11e16fd88104060456ce32751ebf00bdeba01
Andrew Jenkins <jenkins_aj@yahoo.com>
2021-08-16 20:00:26 +0100

    implementing cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const moviesRouter = require("./movies/movies.router")
 const reviewsRouter = require("./reviews/reviews.router")
 const theaterRouter = require("./theaters/theaters.router")
+
+app.use(cors())
 
 app.use(express.json())
 
